db62ece1a44ae1cc986271e4f1560b770ed3999c
Robert Andersson <inputfalken@gmail.com>
2017-01-30 01:14:49 +0100

    Init

---
+++ Netus/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Netus {
+    internal class Program {
+
+        public static void Main(string[] args) {
+            new Thread(Server.StartListening).Start();
+            Console.WriteLine("Out");
+        }
+    }
+}
---
+++ Netus/Server.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Netus {
+    internal static class Server {
+        private static readonly Socket Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private const int BufferSize = 1024;
+        private static readonly AutoResetEvent AutoResetEvent = new AutoResetEvent(false);
+
+
+        public static void StartListening() {
+            Socket.Bind(new IPEndPoint(IPAddress.Any, 23000));
+            Socket.Listen(100);
+            while (true) {
+                Socket.BeginAccept(ClientConnects, Socket);
+                AutoResetEvent.WaitOne();
+            }
+        }
+
+        private static void ClientConnects(IAsyncResult ar) {
+            AutoResetEvent.Set();
+            Console.WriteLine("An client has connected");
+            var listenter = (Socket) ar.AsyncState;
+            var handler = listenter.EndAccept(ar);
+            var state = new State(handler);
+            handler.BeginReceive(state.Buffer, 0, BufferSize, 0, ReadClient, state);
+        }
+
+        private static void ReadClient(IAsyncResult ar) {
+            var res = (State) ar.AsyncState;
+            var result = res.Socket.EndReceive(ar);
+            Console.WriteLine(Encoding.ASCII.GetString(res.Buffer, 0, result));
+        }
+
+        private class State {
+            public Socket Socket { get; }
+            public StringBuilder StringBuilder { get; }
+            public byte[] Buffer { get; }
+
+            public State(Socket socket) {
+                Socket = socket;
+                StringBuilder = new StringBuilder();
+                Buffer = new byte[BufferSize];
+            }
+        }
+    }
+}
