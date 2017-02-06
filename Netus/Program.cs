3113288344b67a33a59c8b34e7c63f34b38a12f1
Robert Andersson <inputfalken@gmail.com>
2017-02-06 10:53:22 +0100

    Add webclient method and event method for messageRecieved.

--- Netus/Program.cs
+++ Netus/Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Threading;
 
 namespace Netus {
     internal class Program {
-
         public static void Main(string[] args) {
             new Thread(Server.StartListening).Start();
-            Console.WriteLine("Out");
+            Server.MessageRecieved += OnServerMessageRecieved;
+        }
+
+        private static void OnServerMessageRecieved(string message) {
+            Console.WriteLine(message);
+        }
+
+        private static void WebClient() {
+            using (var wb = new WebClient()) {
+                var downloadString = wb.DownloadString(new Uri("http://en.wikipedia.org/"));
+                Console.WriteLine("Webclient result \n");
+                Console.WriteLine(downloadString.Length);
+            }
         }
     }
 }
