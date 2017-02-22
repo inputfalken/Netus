c2bda3deb7535a735a11d6d77c8686f02c683e92
Robert Andersson <inputfalken@gmail.com>
2017-02-22 01:38:34 +0100

    Replace new thread with Task.Run

--- Netus/Program.cs
+++ Netus/Program.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Netus {
     internal class Program {
         public static void Main(string[] args) {
-            new Thread(TcpListenerServer.Listen).Start();
-            TcpListenerServer.ClientConnects +=  Console.WriteLine;
+            Task.Run(TcpListenerServer.Listen);
+            TcpListenerServer.ClientConnects += Console.WriteLine;
             TcpListenerServer.ClientMessage += Console.WriteLine;
             TcpListenerServer.ClientDisconects += Console.WriteLine;
+            Console.WriteLine("Press Enter to kill the server");
+            Console.ReadLine();
         }
     }
 }
